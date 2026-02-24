15f285522011797291d215fccf785c488f9fc0dd
Tanya Strydom <tanya.strydom@icloud.com>
2026-02-24 10:01:05 +0200

    🐛 export ALL models...

--- src/FoodWebTools.jl
+++ src/FoodWebTools.jl
@@ -12,4 +12,20 @@ export adbm, adbm_parameters
 include(joinpath("generative_models", "lmatrix.jl"))
 export lmatrix
 
+# niche
+include(joinpath("generative_models", "niche_model.jl"))
+export generate_niche_model
+
+# random
+include(joinpath("generative_models", "random_model.jl"))
+export generate_random_model
+
+# cascade
+include(joinpath("generative_models", "cascade_model.jl"))
+export generate_cascade_model
+
+# ltm
+include(joinpath("generative_models", "ltm.jl"))
+export lmatrix
+
 end # module FoodWebTools
