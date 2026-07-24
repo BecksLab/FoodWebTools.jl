b9d621444068fa46950c1dc8f0b626e141748fe4
Tanya Strydom <tanya.strydom@icloud.com>
2026-07-24 15:13:14 +0100

    🧪 upgraded testing

--- test/units/03_downsampling.jl
+++ test/units/03_downsampling.jl
@@ -135,3 +135,170 @@ using Random
     @test a == b
 
 end
+
+function active_species(mat)
+
+    rows = vec(sum(mat, dims=2) .> 0)
+    cols = vec(sum(mat, dims=1) .> 0)
+
+    sum(rows .|| cols)
+
+end
+
+@testset "Species conservation" begin
+
+    result = downsample(
+        TEST_WEB,
+        :degree;
+        target_co=0.15,
+        min_spp_prop=0.8
+    )
+
+
+    @test active_species(result) >= ceil(0.8*5)
+
+end
+
+function connectance(mat)
+    sum(mat) / length(mat)
+end
+
+
+@testset "Connectance decreases" begin
+
+    original = connectance(TEST_WEB)
+
+    for method in [
+        PowerLaw(2.0),
+        Niche(1.0),
+        DegreeProduct(),
+        RandomSampling()
+    ]
+
+        result = downsample(TEST_WEB, method)
+
+        @test connectance(result) <= original
+
+    end
+
+end
+
+@testset "Target connectance" begin
+
+    target = 0.15
+
+
+    for method in [
+        DegreeProduct(),
+        RandomSampling(),
+        PowerLaw(2.0),
+        Niche(1.0)
+    ]
+
+        result = downsample(
+            TEST_WEB,
+            method;
+            target_co=target
+        )
+
+
+        co = connectance(result)
+
+        @test co <= connectance(TEST_WEB)
+
+    end
+
+end
+
+# Now we test the ecology
+
+GENERALIST_WEB = Bool[
+    1 1 1 1 0
+    0 1 0 0 0
+    0 0 1 0 0
+    0 0 0 1 0
+    0 0 0 0 0
+]
+
+@testset "Degree product targets generalists" begin
+
+    removals = 0
+
+    for i in 1:500
+
+        result = downsample(
+            GENERALIST_WEB,
+            DegreeProduct()
+        )
+
+        if result[1, 1] == false
+            removals += 1
+        end
+
+    end
+
+
+    @test removals > 0
+
+end
+
+@testset "Power law assigns higher retention to generalists" begin
+
+    WEB = Bool[
+        1 1 1 1 1
+        0 1 0 0 0
+        0 0 1 0 0
+        0 0 0 1 0
+        0 0 0 0 1
+    ]
+
+    probs = FoodWebTools.Downsampling._retention_probabilities(
+        WEB,
+        PowerLaw(2.0)
+    )
+
+    @test probs[1] > probs[2]
+    @test probs[1] > probs[3]
+    @test probs[1] > probs[4]
+    @test probs[1] > probs[5]
+
+end
+
+@testset "Degree product removes highly connected interactions" begin
+
+    DEGREE_WEB = Bool[
+        1 1 1 1 1
+        1 0 0 0 0
+        1 0 0 0 0
+        1 0 0 0 0
+        1 0 0 0 0
+    ]
+
+    Random.seed!(66)
+
+    high_degree_losses = 0
+    low_degree_losses = 0
+
+
+    for i in 1:1000
+
+        result = downsample(
+            DEGREE_WEB,
+            DegreeProduct()
+        )
+
+
+        if !result[1, 1]
+            high_degree_losses += 1
+        end
+
+        if !result[2, 1]
+            low_degree_losses += 1
+        end
+
+    end
+
+
+    @test high_degree_losses > low_degree_losses
+
+end
